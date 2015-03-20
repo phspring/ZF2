56d740b8aa61343cebd07614ea7a3942403e45ff
tdaved <ttoth.david@gmail.com>
2015-03-20 19:32:31 +0100

    Updated ControllerGenerator

--- src/PhSpring/ZF2/Engine/ControllerGenerator.php
+++ src/PhSpring/ZF2/Engine/ControllerGenerator.php
@@ -9,7 +9,7 @@
 namespace PhSpring\ZF2\Engine;
 
 use Zend\Code\Generator\ClassGenerator;
-use Zend\Code\Generator\DocBlockGenerator;
+use Zend\Code\Generator\MethodGenerator;
 use Zend\Code\Reflection\ClassReflection;
 
 /**
@@ -18,20 +18,51 @@ use Zend\Code\Reflection\ClassReflection;
  * @author tothd
  */
 class ControllerGenerator {
-    protected function getContent($invokable)
+    
+    /**
+     *
+     * @var ClassGenerator
+     */
+    private $generator;
+    
+    public function getContent($invokable)
     {
-        $generated = ClassGenerator::fromReflection(new ClassReflection($invokable));
-        $interfaces = $generated->getImplementedInterfaces();
-        $interfaces[]=GeneratedControllerInterface::class;
-        $generated->setImplementedInterfaces($interfaces);
-        $generated->setDocBlock(new DocBlockGenerator());
+        $this->generator = ClassGenerator::fromReflection(new ClassReflection($invokable));
+        
+        $this->buildInterfaces();
+        $this->buildMethods();
         //$config =$this->getServiceLocator()->get('servicemanager')->get('Config'); 
         //$this->serviceLocator->get('ServiceManager')->get('ServiceManager')->get('ServiceManager')->get('Config');
         //var_dump($config);
-        $generated->setName('phs'.$generated->getName());
+        $this->generator->setName('phs'.$this->generator->getName());
         $extClass = explode('\\',$invokable);
-        $generated->setExtendedClass(end($extClass));
+        $this->generator->setExtendedClass(end($extClass));
         
-        return $generated->generate();
+        return $this->generator->generate();
+    }
+    
+    private function buildInterfaces()
+    {
+        $interfaces = $this->generator->getImplementedInterfaces();
+        $interfaces[]=GeneratedControllerInterface::class;
+        $this->generator->setImplementedInterfaces($interfaces);
+    }
+    
+    private function buildMethods()
+    {
+        foreach ($this->generator->getMethods() as $method)
+        {
+            $body = 'parent::' . $method->getName() . '(';
+            $params = array_map(function ($param) {
+                return '$' . $param->getName();
+            }, $method->getParameters());
+            $body .= implode(', ', $params) . ');';
+            $newMethod = new MethodGenerator();
+            $newMethod->setName($method->getName());
+            $newMethod->setBody($body);
+            
+            $this->generator->removeMethod($method->getName());
+            $this->generator->addMethodFromGenerator($newMethod);
+        }
     }
 }
